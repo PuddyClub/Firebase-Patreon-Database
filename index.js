25f5d61652091fa94f472d87093e30bd6b51a8e2
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-02 11:03:21 -0300

    log update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ patreonModule.start = function (data) {
             const http_page = require('@tinypudding/puddy-lib/http/HTTP-1.0');
 
             // Log New Event
-            logger.log(`Patreon Event in the domain ${theDomain} with the event code "${req.header('x-patreon-signature')}:${req.header('x-patreon-event')}".`, req.query);
+            logger.log(`Patreon Event in the domain ${theDomain} with the event code "${req.header('x-patreon-signature')}:${req.header('x-patreon-event')}".`, req.query, data);
 
             // Exist
             if (theDomain) {
